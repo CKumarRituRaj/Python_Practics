fd223b3eb0bbfe519415d75100bdb44c3bb4a4d0
Kumar Ritu Raj <46402785+Kumar-Ritu-Raj@users.noreply.github.com>
2023-01-17 13:38:46 +0530

    Update Prime_Number.py
    Updated for negative numbers

--- Prime_Number.py
+++ Prime_Number.py
@@ -1,15 +1,17 @@
 flag=0
 a=2
 num=int(input("Enter a number = "))
-if(num==0 or num==1):
+if(num>0):
+    if(num==0 or num==1):
         flag=1
-else:
+    else:
         while(a<=num/2):
                 if(num%a==0):
                         flag=1
                 a=a+1
-if(flag==0):
+    if(flag==0):
         print(num,"is Prime Number")
-else:
+    else:
         print(num,"is not Prime Number")
-        
+else:
+    print(num,"is not Prime Number")
